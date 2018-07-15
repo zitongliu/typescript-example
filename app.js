539e668aa965d8588e73eb8ba952b9b43162b3d9
zitongliu <zitongliu7@gmail.com>
2018-07-15 15:07:41 +1000

    added type examples

--- app.js
+++ app.js
@@ -1,2 +1,41 @@
 "use strict";
-console.log("It works yall!");
+//string
+var myName = "Steve";
+// myName = 28; 
+// number
+var myAge = 99.99;
+// myAge = 'Steve';
+// boolean
+var hasHobbies = true;
+// hasHobbies = 1;
+// assign types
+var myRealAge;
+myRealAge = 99;
+// myRealAge = '99';
+// array
+var hobbies = ["cooking", "sports"];
+console.log(typeof hobbies);
+hobbies = [100];
+// hobbies = 100;
+// tuples
+// let address = ["Superstreet", 99];
+var address = ["Superstreet", 99];
+// let address: [string, number] = [99, "Superstreet"];
+// enum
+var Color;
+(function (Color) {
+    Color[Color["Gray"] = 0] = "Gray";
+    Color[Color["Red"] = 1] = "Red";
+    Color[Color["Green"] = 100] = "Green";
+    Color[Color["Blue"] = 101] = "Blue";
+    Color[Color["Yellow"] = 2] = "Yellow";
+    Color[Color["Purple"] = 3] = "Purple";
+})(Color || (Color = {}));
+;
+var myColor = Color.Purple;
+console.log(myColor);
+// any
+var car = "BMW";
+console.log(car);
+car = { brand: "BMW", series: 3 };
+console.log(car);
--- app.ts
+++ app.ts
@@ -1,1 +1,46 @@
-console.log("It works yall!");
+//string
+let myName: string = "Steve";
+// myName = 28; 
+
+// number
+let myAge = 99.99;
+// myAge = 'Steve';
+
+// boolean
+let hasHobbies = true;
+// hasHobbies = 1;
+
+// assign types
+let myRealAge: number;
+myRealAge = 99;
+// myRealAge = '99';
+
+// array
+let hobbies: any[] = ["cooking", "sports"];
+console.log(typeof hobbies);
+hobbies = [100];
+// hobbies = 100;
+
+// tuples
+// let address = ["Superstreet", 99];
+let address: [string, number] = ["Superstreet", 99];
+// let address: [string, number] = [99, "Superstreet"];
+
+// enum
+enum Color {
+    Gray,           // 0
+    Red,            // 1
+    Green = 100,    // 100
+    Blue,           // 101
+    Yellow = 2,     // 2
+    Purple,         // 3
+};
+
+let myColor: Color = Color.Purple;
+console.log(myColor);
+
+// any
+let car: any = "BMW";
+console.log(car);
+car = { brand: "BMW", series: 3 };
+console.log(car);
